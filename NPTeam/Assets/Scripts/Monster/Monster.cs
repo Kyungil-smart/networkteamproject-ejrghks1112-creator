1cdca50450e54825771f0608f5c644dd27c68dfd
ejrghks1112-creator <ejrghks1112@gmail.com>
2026-04-30 11:51:39 +0900

    [ADD] : ai의 플레이어 넉백기능 추가
    ai 체력을 네트워크 환경에 맞게 변화 int => networkvariable (테스트 필요)

--- NPTeam/Assets/Scripts/Monster/Monster.cs
+++ NPTeam/Assets/Scripts/Monster/Monster.cs
@@ -11,20 +11,20 @@ public class Monster : NetworkBehaviour, IDamagable
     private NavMeshAgent _navmeshAgent;
     private Transform _targetPlayer;
     private int _randomWayPoint;
+    private bool _isCooldown = false;
     
     [Header("몹 체력")]
-    [SerializeField] private int health;
-    public int Health 
-    {
-        get => health; 
-        private set => health = value; 
-    }
+    [SerializeField] private NetworkVariable<int> health;
+    [SerializeField] private int maxHealth;
 
     [Header("순찰 지점")]
     [SerializeField] private Transform[] patrolPoints;
 
     [Header("플레이어 감지 거리")] 
     [SerializeField] private float detectRange;
+
+    [Header("플레이어 넉백 시도 거리")] 
+    [SerializeField] private float knockbackRange;
     
     [Header("감지 시야각")]
     [SerializeField] private float viewAngle;
@@ -34,15 +34,21 @@ public class Monster : NetworkBehaviour, IDamagable
     
     [Header("순찰시 속도")]
     [SerializeField] private float patrolSpeed;
+    
+    [Header("밀어낼 파워")]
+    [SerializeField] private float knockbackPower;
+
+    [Header("넉백 쿨타임")] 
+    [SerializeField] private float knockbackCooltime;
 
     private void Awake() => _navmeshAgent = GetComponent<NavMeshAgent>();
 
     public override void OnNetworkSpawn()
     {
         if (!IsServer) return;
+        health = new NetworkVariable<int>(maxHealth);
         
         SetWayPoint();
-        // StartCoroutine(TargetSearchRoutine());
     }
 
     private void Update()
@@ -50,9 +56,45 @@ public class Monster : NetworkBehaviour, IDamagable
         if (!IsServer) return;
         
         SetTargetPlayer();
+
+        if (_targetPlayer == null)
+        {
+            Patrol();
+            return;
+        }
         
-        if (_targetPlayer != null && FOV()) Chase();
-        else                                   Patrol();
+        float distance = Vector3.Distance(transform.position, _targetPlayer.position);
+        
+        if (_targetPlayer != null && FOV())
+        {
+            Chase();
+            if (distance < knockbackRange && !_isCooldown) StartCoroutine(KnockbackRoution());
+        }
+        else Patrol();
+    }
+
+    private void KnockbackPlayer()
+    {
+        if (!IsServer) return;
+        
+        var player = _targetPlayer.GetComponent<PlayerVehicle>();
+
+        if (player != null)
+        {
+            Vector3 direction = (_targetPlayer.position - transform.position).normalized;
+            direction += Vector3.up * 0.5f;
+            // player.KnockbackClientRpc(direction * knockbackPower);
+        }
+    }
+
+    private IEnumerator KnockbackRoution()
+    {
+        _isCooldown = true;
+        
+        KnockbackPlayer();
+        yield return new WaitForSeconds(knockbackCooltime);
+        
+        _isCooldown = false;
     }
 
     private void SetWayPoint()
@@ -92,7 +134,7 @@ public class Monster : NetworkBehaviour, IDamagable
             Vector3 directionToPlayer = (_targetPlayer.position - transform.position).normalized;
             float angle = Vector3.Angle(transform.forward, directionToPlayer);
 
-            return angle < viewAngle ? true : false;
+            return angle < viewAngle / 2 ? true : false;
         }
         return false;
     }
@@ -117,8 +159,8 @@ public class Monster : NetworkBehaviour, IDamagable
     {
         Gizmos.color = Color.white;
         
-        Vector3 leftBoundary = Quaternion.Euler(0, -viewAngle, 0) * transform.forward;
-        Vector3 rightBoundary = Quaternion.Euler(0, viewAngle, 0) * transform.forward;
+        Vector3 leftBoundary = Quaternion.Euler(0, -viewAngle / 2, 0) * transform.forward;
+        Vector3 rightBoundary = Quaternion.Euler(0, viewAngle / 2, 0) * transform.forward;
 
         if (_targetPlayer != null && FOV()) Gizmos.color = Color.red;
         else                                   Gizmos.color = Color.gold;
@@ -132,6 +174,7 @@ public class Monster : NetworkBehaviour, IDamagable
     // 데미지를 받기 위한 메서드
     public void TakeDamage(int damage)
     {
-        Health -= damage;
+        if (!IsServer) return;
+        health.Value -= damage;
     }
 }
