0afb7c061f03f605c8f15a23eae6bce2d144b848
156Sheong <waladeus@gmail.com>
2026-05-09 00:55:19 +0900

    [UPDATE] 인게임 UI 프리펩 수정 및 스크립트 변수 추가

--- NPTeam/Assets/Scripts/Ui/InGameUI.cs
+++ NPTeam/Assets/Scripts/Ui/InGameUI.cs
@@ -1,14 +1,17 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 public class InGameUI : MonoBehaviour
 {
     [Header("시간 설정 용")]
-    [SerializeField] private float _playTimeer;
-    [SerializeField] private TextMeshProUGUI timerText;
+    [field: SerializeField] public float EndTimer { get; set; } // 외부에서 받아야 하는 최종 시간
+    [SerializeField] private float _currentTimer;   // 현재 시간
+    [SerializeField] private TextMeshProUGUI timerText; // 시간을 표시할 텍스트
 
 
-    [SerializeField] private Transform _goalPosition;
+    [field: SerializeField] public Transform GoalPosition { get; set; }   // 외부에서 받아올 골 위치
+    [field: SerializeField] public List<Transform> PlayersPosition { get; set; } = new List<Transform> { };   // 외부에서 받아올 플레이어들의 위치
    
 
     private void Start()
@@ -28,18 +31,18 @@ public class InGameUI : MonoBehaviour
 
     private void Init()
     {
-        _playTimeer = 0f;
+        _currentTimer = 0f;
     }
 
 
     // 시간 계산
     private void CalculatingTimer()
     {
-        _playTimeer += Time.deltaTime;
+        _currentTimer += Time.deltaTime;
 
-        int minutes = Mathf.FloorToInt(_playTimeer / 60F);
-        int seconds = Mathf.FloorToInt(_playTimeer % 60F);
-        int milliseconds = Mathf.FloorToInt((_playTimeer * 100F) % 100F);
+        int minutes = Mathf.FloorToInt(_currentTimer / 60F);
+        int seconds = Mathf.FloorToInt(_currentTimer % 60F);
+        int milliseconds = Mathf.FloorToInt((_currentTimer * 100F) % 100F);
 
         timerText.text = string.Format($"{0}:{1}:{2}", minutes, seconds, milliseconds);
     }
