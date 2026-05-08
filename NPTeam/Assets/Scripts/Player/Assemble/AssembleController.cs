c3f4b3da55e041658fa26ce24fe7a1ba20f84920
baewhv <baewhv@naver.com>
2026-05-08 12:54:13 +0900

    [FIX] : 합체 함수 개방

--- NPTeam/Assets/Scripts/Player/Assemble/AssembleController.cs
+++ NPTeam/Assets/Scripts/Player/Assemble/AssembleController.cs
@@ -51,18 +51,18 @@ public class AssembleController : NetworkBehaviour
         AddAssemblePartClientRpc(name, num);
         if (_parts.Count >= 4)
         {
-            ChageCutsceneStateClientRpc();
+            ChangeCutsceneStateClientRpc();
         }
     }
 
     [ClientRpc]
     private void AddAssemblePartClientRpc(string name, int num)
     {
-        //_parts.Add(name, GameManager.Instance.GetVehicle(num).GetComponentForm());
+        _parts.Add(name, GameManager.Instance.GetVehicle(num).GetComponentFormMovement);
     }
 
     [ClientRpc]
-    private void ChageCutsceneStateClientRpc()
+    private void ChangeCutsceneStateClientRpc()
     {
         _robotStateMachine.ChangeState(_cutsceneFormState);
     }
--- NPTeam/Assets/Scripts/Player/Assemble/AssembleMovement.cs
+++ NPTeam/Assets/Scripts/Player/Assemble/AssembleMovement.cs
@@ -16,7 +16,7 @@ public class AssembleMovement : MonoBehaviour
         _anim?.CrossFade("Robot_Transform",0.0f);
         foreach (ComponentFormMovement movement in _controller.GetPartsData.Values)
         {
-            //movement.GetAnim?.CrossFade("Robot_Transform",0.0f);
+            movement.GetAnim?.CrossFade("Robot_Transform",0.0f);
         }
     }
 
