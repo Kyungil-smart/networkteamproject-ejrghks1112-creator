6f59db28d58a1cfb9d0d81c697e4f27ac7ec2629
ejrghks1112-creator <ejrghks1112@gmail.com>
2026-05-11 11:08:32 +0900

    [ADD] 인게임 ui에 플레이어 이름 연결

--- NPTeam/Assets/Scripts/Manager/GameManager.cs
+++ NPTeam/Assets/Scripts/Manager/GameManager.cs
@@ -1,10 +1,39 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : SingletonMonoBehaviour<GameManager>
 {
+    [Header("남은 게임 시간")] 
+    [SerializeField] private float _endTime = 300f;
+
+    public float endTime
+    {
+        get => _endTime;
+        set
+        {
+            _endTime = value;
+            OnTimeChange?.Invoke(_endTime);
+        }
+    }
+
+    [Header("점수")] 
+    [SerializeField] private int _score = 0;
+    public int score
+    {
+        get => _score;
+        set
+        {
+            _score = value;
+            OnScoreChange?.Invoke(_score);
+        }
+    }
+
     //각 PC마다 들고 있을 차량.
      [SerializeField] private AssembleController _LeaderVehicle;
+     
+     public event Action<float>  OnTimeChange;
+     public event Action<int>  OnScoreChange;
     
      public AssembleController LeaderVehicle
      {
@@ -32,5 +61,13 @@ public class GameManager : SingletonMonoBehaviour<GameManager>
             PlayerVehicles[num] = vehicle;
         else
             Debug.LogError("중복된 차량 추가");
+    }
+
+    private void Update()
+    {
+        if (endTime > 0)
+        {
+            endTime -= Time.deltaTime;
+        }
     }
 }
