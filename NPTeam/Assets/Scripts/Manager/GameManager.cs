499009d2a470ab40bdf9c15b718a2bb31a46edaa
ejrghks1112-creator <ejrghks1112@gmail.com>
2026-05-11 11:23:08 +0900

    [ADD] 인게임 ui에 플레이어 이름 연결

--- NPTeam/Assets/Scripts/Manager/GameManager.cs
+++ NPTeam/Assets/Scripts/Manager/GameManager.cs
@@ -1,8 +1,34 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : SingletonMonoBehaviour<GameManager>
 {
+    [Header("남은 게임 시간")] 
+    [SerializeField] private float endTime = 300f;
+
+    public float EndTime
+    {
+        get => endTime;
+        set
+        {
+            endTime = value;
+            OnTimeChange?.Invoke(endTime);
+        }
+    }
+    
+    [Header("점수")] 
+    [SerializeField] private int score = 0;
+    public int Score
+    {
+        get => score;
+        set
+        {
+            score = value;
+            OnScoreChange?.Invoke(score);
+        }
+    }
+    
     //각 PC마다 들고 있을 차량.
      [SerializeField] private AssembleController _LeaderVehicle;
     
@@ -11,10 +37,22 @@ public class GameManager : SingletonMonoBehaviour<GameManager>
          get => _LeaderVehicle;
          set => _LeaderVehicle = value;
      }
+     
+     public event Action<float>  OnTimeChange;
+     public event Action<int>  OnScoreChange;
+
 
     
     private Dictionary<int, PlayerVehicle> PlayerVehicles = new();
     public int GetVehiclesNum => PlayerVehicles.Count;
+    
+    private void Update()
+    {
+        if (endTime > 0)
+        {
+            endTime -= Time.deltaTime;
+        }
+    }
 
     public PlayerVehicle GetVehicle(int num)
     {
--- NPTeam/Assets/Scripts/Ui/InGameUI.cs
+++ NPTeam/Assets/Scripts/Ui/InGameUI.cs
@@ -1,54 +1,117 @@
 using System.Collections.Generic;
 using TMPro;
+using Unity.Collections;
+using Unity.Netcode;
 using UnityEngine;
 
-public class InGameUI : MonoBehaviour
+public class InGameUI : NetworkBehaviour
 {
     [Header("시간 설정 용")]
     [field: SerializeField] public float EndTimer { get; set; } // 외부에서 받아야 하는 최종 시간
     [SerializeField] private float _currentTimer;   // 현재 시간
     [SerializeField] private TextMeshProUGUI timerText; // 시간을 표시할 텍스트
-
-
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI playerNameText;
+    
+    [Header("다른 플레이어 이름")]
+    [SerializeField] private List<GameObject> otherPlayers;
+    
+    private int index = 0;
+    public NetworkList<FixedString32Bytes> playerName;
+    
     [field: SerializeField] public Transform GoalPosition { get; set; }   // 외부에서 받아올 골 위치
     [field: SerializeField] public List<Transform> PlayersPosition { get; set; } = new List<Transform> { };   // 외부에서 받아올 플레이어들의 위치
-   
-
-    private void Start()
+    
+    private void Awake()
     {
-        Init();
+        playerName = new NetworkList<FixedString32Bytes>();
     }
 
-    private void Update()
+    public override void OnNetworkSpawn()
     {
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.OnTimeChange += UpdateTime;
+            // GameManager.Instance.OnScoreChange += UpdateScore;
+
+            UpdateTime(GameManager.Instance.EndTime);
+            // UpdateScore(GameManager.Instance.Score);
+        }
         
-        CalculatingTimer(); // 시간 계산
+        playerName.OnListChanged += CheckPlayerList;
+        
+        if (IsClient)
+        {
+            NameBroadcastServerRpc(LobbyManager.Instance.PlayerName); 
+        }
+        
+        UpdatePlayerList();
     }
 
-
-
-
-
-    private void Init()
+    public override void OnNetworkDespawn()
     {
-        _currentTimer = 0f;
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.OnTimeChange -= UpdateTime;
+            // GameManager.Instance.OnScoreChange -= UpdateScore;
+        }
+        
+        playerName.OnListChanged -= CheckPlayerList;
     }
 
-
-    // 시간 계산
-    private void CalculatingTimer()
+    private void CheckPlayerList(NetworkListEvent<FixedString32Bytes> changeEvent)
     {
-        _currentTimer += Time.deltaTime;
-
-        int minutes = Mathf.FloorToInt(_currentTimer / 60F);
-        int seconds = Mathf.FloorToInt(_currentTimer % 60F);
-        int milliseconds = Mathf.FloorToInt((_currentTimer * 100F) % 100F);
-
-        timerText.text = string.Format($"{0}:{1}:{2}", minutes, seconds, milliseconds);
+        UpdatePlayerList();
     }
 
+    private void UpdatePlayerList()
+    {
+        foreach (var panel in otherPlayers) panel.SetActive(false);
+        
+        string myName = LobbyManager.Instance.PlayerName;
+        playerNameText.text = myName;
 
+        int index = 0;
 
+        foreach (var name in playerName)
+        {
+            string otherName = name.ToString();
+            
+            if (otherName == myName) continue;
+            
+            if (index < otherPlayers.Count)
+            {
+                otherPlayers[index].SetActive(true);
+                var text = otherPlayers[index].GetComponentInChildren<TextMeshProUGUI>();
+                if (text != null) text.text = otherName;
+                
+                index++;
+            }
+        }
+    }
 
+    private void UpdateTime(float time)
+    {
+        float currentTime = Mathf.Max(0, time);
 
- }
+        int minutes = Mathf.FloorToInt(currentTime / 60F);
+        int seconds = Mathf.FloorToInt(currentTime % 60F);
+        int milliseconds = Mathf.FloorToInt((currentTime * 100F) % 100F);
+
+        timerText.text = string.Format("{0}:{1}:{2}", minutes, seconds, milliseconds);
+    }
+
+    private void UpdateScore(int score)
+    {
+        scoreText.text = $"Score: {score}";
+    }
+
+    [Rpc(SendTo.Server, InvokePermission = RpcInvokePermission.Everyone)]
+    private void NameBroadcastServerRpc(string myName)
+    {
+        if (!playerName.Contains(myName))
+        {
+            playerName.Add(myName);
+        }
+    }
+}
