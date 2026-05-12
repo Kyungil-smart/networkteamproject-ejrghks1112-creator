6b9c48a5e3ff0159d40e3471f23180d6a585c21e
Geunhyung Song <248142774+gamyeonx@users.noreply.github.com>
2026-05-12 22:31:48 +0900

    [UPDATE] 아이스크림봇 잽 공격 구현

--- NPTeam/Assets/Scripts/Player/RobotForm/RobotZab.cs
+++ NPTeam/Assets/Scripts/Player/RobotForm/RobotZab.cs
@@ -4,11 +4,16 @@ using UnityEngine.InputSystem;
 
 public class RobotZab : NetworkBehaviour
 {
-    private InputAction _playerZab;
     [Header("부모 객체인 PlayerVehicle를 참조")]
     [SerializeField] private GameObject _playerVehicle;
     [SerializeField] private PlayerVehicle _playerVehicleCS;
     [SerializeField] private Animator _animator;
+    [Header("로봇 왼손 끝 등록(레이를 위한 피봇)")]
+    [SerializeField] private SphereCollider _zabCollider;
+    [Header("때리기 가능한 레이어마스크 등록")]
+    [SerializeField] private LayerMask _canZabLayer;
+
+    private InputAction _playerZab;
 
     private void Awake() => Init();
 
@@ -31,6 +36,32 @@ public class RobotZab : NetworkBehaviour
     {
         if (!IsOwner) return;
         if (!ctx.started || PlayerState.Instance.IsPossession == false || PlayerState.Instance.CurrentPossessed != _playerVehicle) return;
+        if (_playerVehicleCS.Stamina < 10) return;
         _animator.SetTrigger("IsZab");
+        _playerVehicleCS.ChangeStamina(-10);
+    }
+
+    public void ZabAttack()
+    {
+        Collider[] hits = Physics.OverlapSphere(_zabCollider.transform.position, _zabCollider.radius, _canZabLayer);
+
+        foreach (Collider hit in hits)
+        {
+            Monster monster = hit.GetComponent<Monster>();
+
+            if (monster != null)
+            {
+                monster.TakeDamage(100);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (_zabCollider == null) return;
+
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawWireSphere(_zabCollider.transform.position, _zabCollider.radius);
     }
 }
